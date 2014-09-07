015252588e5e4544a163131dcd63a464f592fb91
Michael Rodrigues <mikebrodrigues@gmail.com>
2014-09-07 13:34:19 -0700

    trying to fix sorting of IPs

--- app.rb
+++ app.rb
@@ -10,6 +10,15 @@ set :public, 'public'
 
 
 $config = YAML.load(File.read('./config.yml'))
+@scan_table =  []
+$config.each do |id, attrs|
+  entry = []
+  entry << id
+  attrs.each do |k, v|
+    entry << v
+  end
+  @scan_table << entry
+end
 
 get '/' do
 
