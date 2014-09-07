f58ee725f923b50467abbc599117fa16b9c7fd47
Michael Rodrigues <mikebrodrigues@gmail.com>
2014-09-07 03:23:05 -0700

    first commit, basic functionality

---
+++ app.rb
@@ -0,0 +1,15 @@
+require 'arp_scan'
+require 'json'
+require 'sinatra'
+require 'haml'
+
+get '/' do
+  haml :table
+end
+
+get '/scan_report.json' do
+  content_type :json
+
+  ARPScan('-l').to_hash[:hosts].to_json
+
+end
