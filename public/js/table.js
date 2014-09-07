cfe3558348bc3e34df2b2dad98a32c2cf8af7895
Michael Rodrigues <mikebrodrigues@gmail.com>
2014-09-07 08:27:25 -0700

    color changes, fixed IP sorting

--- public/js/table.js
+++ public/js/table.js
@@ -11,23 +11,53 @@ $( document ).ready(function() {
 
 	$.getJSON("/scan_report.json", callback);
 
+	$.tablesorter.addParser({
+		id: 'ip',
+		is: function(s) {
+			return false;
+		},
+		format: function(s) {
+			s = $.trim(s);
+			s = s.replace(/\s+/gm, ',');
+			var arrIp = s.split(',');
+			for (var count = 0; count < arrIp.length; count++) {
+				if (-1 != arrIp[count].search('-')) {
+					var arrIpRange = arrIp[count].split('-');
+					arrIpRange[0] = padIp(arrIpRange[0]);
+					arrIpRange[1] = padIp(arrIpRange[1]);
+					arrIp[count] = arrIpRange[0] + '-' + arrIpRange[1];
+				} else if (-1 != arrIp[count].search('/')) {
+					var arrIpMasked = arrIp[count].split('/');
+					arrIpMasked[0] = padIp(arrIpMasked[0]);
+					arrIpMasked[1] = padIp(arrIpMasked[1]);
+					arrIp[count] = arrIpMasked[0] + '/' + arrIpMasked[1];
+				} else {
+					arrIp[count] = padIp(arrIp[count]);
+				}
+			}
+			s = arrIp.join(',');
+			return s;
+		},
+		type: 'text'
+	});
+
 	function callback(data)
-{
-	var info=[];
-	info.push(data);
-	console.log(info);
-	console.log(data.hosts);
-	var hosts_options = {
-		source: data.hosts,
-		callback: function(){
-			$("#hostsTable").tablesorter();
+	{
+		var info=[];
+		info.push(data);
+		console.log(info);
+		console.log(data.hosts);
+		var hosts_options = {
+			source: data.hosts,
+			callback: function(){
+				$("#hostsTable").tablesorter();
+			}
 		}
+		var info_options = {
+			source: info
+		}
+		$("#hostsTable").jsonTableUpdate(hosts_options);
+		$("#infoTable").jsonTableUpdate(info_options);
 	}
-	var info_options = {
-		source: info
-	}
-	$("#hostsTable").jsonTableUpdate(hosts_options);
-	$("#infoTable").jsonTableUpdate(info_options);
-}
 });
 
